a6bfd15629a703cddb85e2187bd743dd8d97b25d
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-22 09:38:40 -0700

    add model and services folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './component/pages/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

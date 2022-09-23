ca25b04ffcce7257e7f5766b4509c88696cba754
john <johhab10@gmail.com>
2022-09-23 05:18:35 -0400

    added create page with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCardComponent } from './pages/create-card/create-card.component';
+import { CreateNotesComponent } from './pages/create-notes/create-notes.component';
+import { ListCardsComponent } from './pages/list-cards/list-cards.component';
+import { ListNotesComponent } from './pages/list-notes/list-notes.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCardComponent,
+    CreateNotesComponent,
+    ListCardsComponent,
+    ListNotesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/list-cards/list-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cards',
+  templateUrl: './list-cards.component.html',
+  styleUrls: ['./list-cards.component.css']
+})
+export class ListCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

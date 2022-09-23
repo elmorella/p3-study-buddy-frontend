d9efae19a68b3943403caaa1f91d2126388b2687
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-22 21:45:48 -0700

    style card list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/home/header/header.component';
+import { ButtonSetComponent } from './components/home/button-set/button-set.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ListCardComponent } from 'src/app/components/list-card/list-card.component';
 import { ListNotesComponent } from 'src/app/components/list-notes/list-notes.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomePageComponent,    
-  },
-  {path:'card', 
-  component: CardComponent}
-
-  { path: '', component: ListCardComponent},
-  { path: 'cardlist', component: ListCardComponent},
-  { path: 'noteslist', component: ListNotesComponent}
-];
+  { path: '', component: ListCardComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'card', component: CardComponent },
+  { path: 'cardlist', component: ListCardComponent },
+  { path: 'noteslist', component: ListNotesComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -24,7 +23,12 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponents = [
+  HomePageComponent,
+  NavbarComponent,
+  HeaderComponent,
+  ButtonSetComponent,
+  FooterComponent,
+  CardComponent,
   ListCardComponent,
-  ListNotesComponent,
-  CardComponent
+  ListNotesComponent
 ]

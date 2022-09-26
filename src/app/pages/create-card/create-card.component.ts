fe45ccede6451887be11fb7efa1d83dc10fd6746
john <johhab10@gmail.com>
2022-09-26 17:02:45 -0400

    styled form

--- src/app/pages/create-card/create-card.component.ts
+++ src/app/pages/create-card/create-card.component.ts
@@ -2,25 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Card } from 'src/app/model/card.model';
 import { Deck } from 'src/app/model/deck.model';
 
+
 @Component({
   selector: 'app-create-card',
   templateUrl: './create-card.component.html',
   styleUrls: ['./create-card.component.css']
 })
 export class CreateCardComponent implements OnInit {
-  deckList: Deck[]=[
-   
+  deckList: Deck[]=[];
+  cardList: Array<Card> = [];
 
-  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addCardtoSet(forms:any){
+
+  }
   addDecktoList(forms:any){
-    let tempDeck = new Deck();
-    tempDeck.title = forms.value.title;
-    tempDeck.description = forms.value.description;
-    this.deckList.push(tempDeck);
+    if (!forms.value.title || !forms.value.description){
+
+    }
+    else{
+      let tempDeck = new Deck();
+      tempDeck.title = forms.value.title;
+      tempDeck.description = forms.value.description;
+      this.deckList.push(tempDeck);
+    }
+    
   }
 }

d93bc6eba472604c5e40667cdbf65b7c511df1c5
Elmer Orellana <104410892+ssblue18@users.noreply.github.com>
2022-09-28 23:47:44 -0700

    instal CKEditor for Rich Text Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,23 @@ import { CardService } from './services/card.service';
 import { DeckService } from './services/deck.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { CKEditorModule } from 'ng2-ckeditor'
+import { CKEditorModule } from 'ckeditor4-angular';
 import { MatIconModule } from '@angular/material/icon'
 import { HttpClientModule } from '@angular/common/http';
-import { SearchNotesComponent } from './components/search-notes/search-notes.component';
 import { CreateNotesComponent } from './components/create-notes/create-notes.component';
-import {MatDialogModule, MatDialogConfig, MatDialog} from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    SearchNotesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatDialogModule,
     CKEditorModule,
     FormsModule,
     MatIconModule,
